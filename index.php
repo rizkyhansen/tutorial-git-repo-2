a3b4963db7272e67f6af76fa38b6337a83e0a556
rizkyhansen <rizkyhansenn@gmail.com>
2024-12-28 21:27:21 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hellow world</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur praesentium, officia consectetur, optio deserunt omnis expedita odio nulla temporibus, molestias dolorum dicta libero aspernatur placeat. Nostrum inventore exercitationem sed rerum?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
